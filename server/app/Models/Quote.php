e8374d3cb0b933399026c3e57da817ee1071b40f
Marvin Dosse <marvin@dosse.org>
2023-02-16 17:00:36 +0100

    Create quotes table and add Quote model with fillable fields.

---
+++ server/app/Models/Quote.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Quote extends Model
+{
+    protected $fillable = [
+        'quote',
+        'character',
+        'image',
+        'characterDirection'
+    ];
+}
