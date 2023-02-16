d8cb84bf4b3a1813e1c63fc5736dc816440c510c
Marvin Dosse <marvin@dosse.org>
2023-02-16 22:24:37 +0100

    Update theme extend with Simpsons yellow color.

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -2,7 +2,11 @@ const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				'simpsons-yellow': '#ffde00'
+			}
+		}
 	},
 
 	plugins: []
