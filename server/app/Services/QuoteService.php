c90f23f6b668716978aaefbf215fd9489724b17b
Marvin Dosse <marvin@dosse.org>
2023-02-16 19:30:38 +0100

    Add QuoteService and update QuotesController to use it for fetching and managing quotes

---
+++ server/app/Services/QuoteService.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Quote;
+use Illuminate\Support\Facades\Http;
+
+class QuoteService
+{
+    public function getQuotes()
+    {
+        $quote = $this->getQuoteFromApi();
+        $quotes = Quote::all();
+
+        // If we have more than 5 quotes, delete the oldest one
+        if ($quotes->count() >= 5) {
+            $oldestQuote = $quotes->sortBy('created_at')->first();
+            $oldestQuote->delete();
+        }
+
+        return $quotes;
+    }
+
+    public function getQuoteFromApi()
+    {
+        $response = Http::get('https://thesimpsonsquoteapi.glitch.me/quotes');
+        $quote = $response->json()[0];
+
+        Quote::create([
+            'quote' => $quote['quote'],
+            'character' => $quote['character'],
+            'image' => $quote['image'],
+            'characterDirection' => $quote['characterDirection']
+        ]);
+
+        return $quote;
+    }
+}
